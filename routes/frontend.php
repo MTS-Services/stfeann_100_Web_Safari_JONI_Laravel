b4bfdb947c8666e9502c780a0647e90c04714f50
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-07-08 17:15:06 +0600

    Update frontend.php

--- routes/frontend.php
+++ routes/frontend.php
@@ -9,5 +9,7 @@ Route::group(['as' => 'f.'], function () {
     Route::get('/detail/{slug}', [HomeController::class, 'detail'])->name('detail');
     Route::get('/shop', [HomeController::class, 'shop'])->name('shop');
     Route::get('/about', [HomeController::class, 'about'])->name('about');
+    // search 
+    Route::get('/search', [SearchController::class, 'search'])->name('products.search');
 
 });
