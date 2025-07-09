6df483373a7e004de0361b1ef5492381f213fc33
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-07-09 15:14:55 +0600

    Update header.blade.php

--- resources/views/components/frontend/header.blade.php
+++ resources/views/components/frontend/header.blade.php
@@ -4,7 +4,7 @@
 </style>
 <section class="bg-text-secondary">
     <nav class="bg-white shadow-md fixed top-0 w-full z-50">
-        <div class="max-w-[1720px] mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-6 lg:py-8">
+        <div class="max-w-[1650px] mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-6 lg:py-8">
             <div class="flex items-center justify-between h-10">
 
                 <div class="md:hidden">
