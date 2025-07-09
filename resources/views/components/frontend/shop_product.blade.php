6f5ce34dbe8e001694a1884cd56eaeafcc85433c
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-07-09 12:19:11 +0600

    update

--- resources/views/components/frontend/shop_product.blade.php
+++ resources/views/components/frontend/shop_product.blade.php
@@ -33,5 +33,18 @@
             </div>
             <span class="text-gray-600 text-[12px]">(4.0)</span>
         </div>
+         <div class="flex justify-between items-center mt-3">
+            <h4 class="text-2xl font-extrabold text-black">{{ $product->price }}€</h4>
+            <button
+                class="bg-red-500 text-white rounded-lg px-4 py-2 text-base font-semibold group-hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition-colors flex items-center gap-2 shadow relative">
+                <a class="absolute inset-0" href="Javascript:void(0);"></a>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2 9m5-9v9m4-9v9m4-9l2 9" />
+                </svg>
+                <span class="hidden lg:inline">Add to Cart</span>
+            </button>
+        </div>
     </div>
 </div>
